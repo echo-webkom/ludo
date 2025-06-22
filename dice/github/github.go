7031fa6140c454877c5740ecb444f1bd3404a075
jesperkha <hammerjesp@gmail.com>
2025-06-22 17:25:49 +0200

    Pull out pr into own type

--- dice/github/github.go
+++ dice/github/github.go
@@ -9,17 +9,22 @@ import (
 )
 
 type Branch struct {
-	Name    string // Name of branch
-	Owner   string // Owner of repo (eg. echo-webkom)
-	Repo    string // Repo name
-	Creator string // Username of user that created the branch
-	PROpen  bool   // If a PR is open or not
-	PRLink  string // URL to PR, else empty
-	Merged  bool   // If the branch is merged
-	Commits int    // Number of commits ahead of main
-	Draft   bool   // If the PR is draft
+	Name    string       // Name of branch
+	Owner   string       // Owner of repo (eg. echo-webkom)
+	Repo    string       // Repo name
+	Commits int          // Number of commits ahead of main
+	PR      *PullRequest // nil if none are open
 }
 
+type PullRequest struct {
+	Creator string // Username of user that created the pr
+	Open    bool   // If the pr is open or not
+	Url     string // URL to pr
+	Draft   bool   // If the pr is draft
+	Merged  bool   // If the pr is merged
+}
+
+// Get html url to branch at github
 func (b Branch) URL() string {
 	return fmt.Sprintf("https://github.com/%s/%s/tree/%s", b.Repo, b.Owner, b.Name)
 }
@@ -35,7 +40,7 @@ func New(config *config.Config) *Client {
 
 // Fetch info on given branch in repo with owner. Returns error if branch does
 // not exist or the request failed.
-func (c *Client) FetchBranchInfo(user, repoOwner, repoName, branchName string) (branch Branch, err error) {
+func (c *Client) FetchBranchInfo(repoOwner, repoName, branchName string) (branch Branch, err error) {
 	ctx := context.Background()
 	repo, _, err := c.client.Repositories.Get(ctx, repoOwner, repoName)
 	if err != nil {
@@ -49,31 +54,32 @@ func (c *Client) FetchBranchInfo(user, repoOwner, repoName, branchName string) (
 
 	prs, _, err := c.client.PullRequests.List(ctx, repoOwner, repoName, &github.PullRequestListOptions{
 		State: "open",
-		Head:  fmt.Sprintf("%s:%s", user, branchName),
+		Head:  fmt.Sprintf("%s:%s", repoOwner, branchName),
 	})
 	if err != nil {
 		return branch, err
 	}
 
-	prUrl, prCreator, prDraft, prOpen, prMerged := prInfo(prs)
-
 	return Branch{
 		Name:    branchName,
 		Owner:   repoOwner,
 		Repo:    repoName,
-		Creator: prCreator,
-		PRLink:  prUrl,
-		PROpen:  prOpen,
-		Draft:   prDraft,
-		Merged:  prMerged,
 		Commits: compare.GetAheadBy(),
+		PR:      prInfo(prs),
 	}, nil
 }
 
-func prInfo(prs []*github.PullRequest) (url, creator string, isDraft, isOpen, isMerged bool) {
+func prInfo(prs []*github.PullRequest) *PullRequest {
 	if len(prs) == 0 {
-		return
+		return nil
 	}
+
 	pr := prs[0]
-	return pr.GetURL(), pr.GetUser().GetName(), pr.GetDraft(), pr.ClosedAt != nil, pr.GetMerged()
+	return &PullRequest{
+		Url:     pr.GetHTMLURL(),
+		Creator: pr.GetUser().GetLogin(),
+		Draft:   pr.GetDraft(),
+		Open:    true,
+		Merged:  pr.GetMerged(),
+	}
 }
