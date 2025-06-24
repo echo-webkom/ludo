eeef53382aa4fc7e146bbe482e33b25f00b939f7
jesperkha <hammerjesp@gmail.com>
2025-06-24 19:35:32 +0200

    clean up database package

--- dice/database/query.go
+++ dice/database/query.go
@@ -4,24 +4,7 @@ import (
 	"errors"
 )
 
-type Database interface {
-	GetUserById(id uint) (User, error)
-	CreateUser(name string) error
-	DeleteUserById(id uint) error
-	GetAllUsers() ([]User, error)
-	GetItemById(id uint) (Item, error)
-	CreateItem(title, description string, repo Repo, creator User, conectedBranch string, list List)
-	GetAllItems() ([]Item, error)
-	DeleteItemByID(id uint) error
-	GetAllItemsFromLits(list List) ([]Item, error)
-	MoveItemToList(id uint, list List) error
-	ChangeItemTitle(id uint, title string) error
-	ChangeItemDescription(id uint, description string) error
-	CreateNewRepo(name, url string) error
-	DeleteRepoById(id uint) error
-}
-
-func (db *TursoDB) GetUserById(id uint) (User, error) {
+func (db *tursoDB) GetUserById(id uint) (User, error) {
 	var user User
 	if res := db.db.First(&user, id); res.Error != nil {
 		return User{}, errors.New("could not find usre with id")
@@ -29,21 +12,21 @@ func (db *TursoDB) GetUserById(id uint) (User, error) {
 	return user, nil
 }
 
-func (db *TursoDB) CreateUser(name string) error {
+func (db *tursoDB) CreateUser(name string) error {
 	if res := db.db.Create(User{Name: name}); res.Error != nil {
 		return errors.New("could not create a new user")
 	}
 	return nil
 }
 
-func (db *TursoDB) DeleteUserById(id uint) error {
+func (db *tursoDB) DeleteUserById(id uint) error {
 	if res := db.db.Delete(&User{}, id); res.Error != nil {
 		return errors.New("could not delete user")
 	}
 	return nil
 }
 
-func (db *TursoDB) GetAllUsers() ([]User, error) {
+func (db *tursoDB) GetAllUsers() ([]User, error) {
 	var users []User
 	if res := db.db.Find(&users); res.Error != nil {
 		return nil, errors.New("courl not get all userts")
@@ -51,7 +34,7 @@ func (db *TursoDB) GetAllUsers() ([]User, error) {
 	return users, nil
 }
 
-func (db *TursoDB) GetItemById(id uint) (Item, error) {
+func (db *tursoDB) GetItemById(id uint) (Item, error) {
 	var item Item
 	if res := db.db.First(&item, id); res.Error != nil {
 		return Item{}, errors.New("could not get item")
@@ -59,15 +42,14 @@ func (db *TursoDB) GetItemById(id uint) (Item, error) {
 	return item, nil
 }
 
-func (db *TursoDB) CreateItem(title, description string, repo Repo, creator User, connectedBranch string, list List) error {
-	item := Item{Title: title, Description: description, Repo: repo, Creator: creator, ConnectedBranch: connectedBranch, List: list, Assignee: User{}}
+func (db *tursoDB) CreateItem(item Item) error {
 	if res := db.db.Create(&item); res.Error != nil {
 		return errors.New("could not create	item")
 	}
 	return nil
 }
 
-func (db *TursoDB) GetAllItems() ([]Item, error) {
+func (db *tursoDB) GetAllItems() ([]Item, error) {
 	var items []Item
 	if res := db.db.Find(&items); res.Error != nil {
 		return nil, errors.New("could not get all items")
@@ -75,14 +57,14 @@ func (db *TursoDB) GetAllItems() ([]Item, error) {
 	return items, nil
 }
 
-func (db *TursoDB) DeleteItemByID(id uint) error {
+func (db *tursoDB) DeleteItemByID(id uint) error {
 	if res := db.db.Delete(&Item{}, id); res.Error != nil {
 		return errors.New("could not delete item")
 	}
 	return nil
 }
 
-func (db *TursoDB) GetAllItemsFromLits(list List) ([]Item, error) {
+func (db *tursoDB) GetAllItemsFromLits(list List) ([]Item, error) {
 	var items []Item
 	if res := db.db.Find(&items, "list = ?", list); res.Error != nil {
 		return nil, errors.New("could not find all items from list")
@@ -90,28 +72,28 @@ func (db *TursoDB) GetAllItemsFromLits(list List) ([]Item, error) {
 	return items, nil
 }
 
-func (db *TursoDB) MoveItemToList(id uint, list List) error {
+func (db *tursoDB) MoveItemToList(id uint, list List) error {
 	if res := db.db.Model(&Item{}).Where("id = ?", id).Update("list", list); res.Error != nil {
 		return errors.New("could not move item")
 	}
 	return nil
 }
 
-func (db *TursoDB) ChangeItemTitle(id uint, title string) error {
+func (db *tursoDB) ChangeItemTitle(id uint, title string) error {
 	if res := db.db.Model(&Item{}).Where("id = ?", id).Update("title", title); res.Error != nil {
 		return errors.New("could not change item title")
 	}
 	return nil
 }
 
-func (db *TursoDB) ChangeItemDescription(id uint, description string) error {
+func (db *tursoDB) ChangeItemDescription(id uint, description string) error {
 	if res := db.db.Model(&Item{}).Where("id = ?", id).Update("description", description); res.Error != nil {
 		return errors.New("could not change item description")
 	}
 	return nil
 }
 
-func (db *TursoDB) CreateNewRepo(name, url string) error {
+func (db *tursoDB) CreateNewRepo(name, url string) error {
 	repo := Repo{Name: name, URL: url}
 	if res := db.db.Create(&repo); res.Error != nil {
 		return errors.New("could not create new user")
@@ -119,7 +101,7 @@ func (db *TursoDB) CreateNewRepo(name, url string) error {
 	return nil
 }
 
-func (db *TursoDB) DeleteRepoById(id uint) error {
+func (db *tursoDB) DeleteRepoById(id uint) error {
 	if res := db.db.Delete(&Repo{}, id); res.Error != nil {
 		return errors.New("could not delet the repo")
 	}
