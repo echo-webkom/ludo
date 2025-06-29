083fa9ec2e2b9f58163c6bb829480fb61e7cef32
jesperkha <hammerjesp@gmail.com>
2025-06-29 14:55:15 +0200

    Add all requests in Service

--- board/service/service.go
+++ board/service/service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"path"
@@ -24,7 +25,7 @@ func New() *LudoService {
 // should already be formatted. body is the request body, a reference to the
 // object you want to encode as json, nil for empty body. response is a
 // reference to the response type, or nil for no response.
-func (l *LudoService) request(method string, endpoint string, body any, response any) error {
+func (l *LudoService) request(method, endpoint string, body, response any) error {
 	var bodyReader *bytes.Reader
 	if body != nil {
 		bodyJson, err := json.Marshal(body)
@@ -63,19 +64,22 @@ func (l *LudoService) GetAllUsers() (users []database.User, err error) {
 }
 
 func (l *LudoService) CreateUser(user database.User) (id uint, err error) {
+	// TODO: add response ID struct
+	err = l.request("POST", "/users", &user, nil)
 	return id, err
 }
 
 func (l *LudoService) GetUserById(id uint) (user database.User, err error) {
+	err = l.request("GET", fmt.Sprintf("/users/%d", id), nil, &user)
 	return user, err
 }
 
 func (l *LudoService) UpdateUser(id uint, user database.User) error {
-	return nil
+	return l.request("PATCH", fmt.Sprintf("/users/%d", id), &user, nil)
 }
 
 func (l *LudoService) DeleteUser(id uint) error {
-	return nil
+	return l.request("DELETE", fmt.Sprintf("/users/%d", id), nil, nil)
 }
 
 func (l *LudoService) GetAllBoards() (boards []database.Board, err error) {
@@ -83,39 +87,45 @@ func (l *LudoService) GetAllBoards() (boards []database.Board, err error) {
 	return boards, err
 }
 
-func (l *LudoService) CreateBoard(board database.Board) error {
-	return nil
+func (l *LudoService) CreateBoard(board database.Board) (id uint, err error) {
+	// TODO: add response ID struct
+	err = l.request("POST", "/boards", &board, nil)
+	return id, err
 }
 
 func (l *LudoService) GetBoardById(id uint) (board database.Board, err error) {
+	err = l.request("GET", fmt.Sprintf("/boards/%d", id), nil, &board)
 	return board, err
 }
 
 func (l *LudoService) UpdateBoard(id uint, board database.Board) error {
-	return nil
+	return l.request("PATCH", fmt.Sprintf("/boards/%d", id), &board, nil)
 }
 
 func (l *LudoService) DeleteBoard(id uint) error {
-	return nil
+	return l.request("DELETE", fmt.Sprintf("/boards/%d", id), nil, nil)
 }
 
 func (l *LudoService) GetAllUsersInBoard(id uint) (users []database.User, err error) {
+	err = l.request("GET", fmt.Sprintf("/boards/%d/users", id), nil, &users)
 	return users, err
 }
 
 func (l *LudoService) GetAllItemsInBoard(id uint) (items []database.Item, err error) {
+	err = l.request("GET", fmt.Sprintf("/boards/%d/items", id), nil, &items)
 	return items, err
 }
 
 func (l *LudoService) AddUserToBoard(boardId, userId uint) error {
-	return nil
+	return l.request("POST", fmt.Sprintf("/boards/%d/users/%d", boardId, userId), nil, nil)
 }
 
 func (l *LudoService) RemoveUserFromBoard(boardId, userId uint) error {
-	return nil
+	return l.request("DELETE", fmt.Sprintf("/boards/%d/users/%d", boardId, userId), nil, nil)
 }
 
 func (l *LudoService) GetAllItemsInBoardList(boardId, list uint) (items []database.Item, err error) {
+	err = l.request("GET", fmt.Sprintf("/boards/%d/lists/%d/items", boardId, list), nil, &items)
 	return items, err
 }
 
@@ -125,21 +135,24 @@ func (l *LudoService) GetAllItems() (items []database.Item, err error) {
 }
 
 func (l *LudoService) CreateItem(item database.Item) (id uint, err error) {
+	// TODO: add response ID struct
+	err = l.request("POST", "/items", &item, nil)
 	return id, err
 }
 
 func (l *LudoService) GetItemById(id uint) (item database.Item, err error) {
+	err = l.request("GET", fmt.Sprintf("/items/%d", id), nil, &item)
 	return item, err
 }
 
 func (l *LudoService) UpdateItem(id uint, item database.Item) error {
-	return nil
+	return l.request("PATCH", fmt.Sprintf("/items/%d", id), &item, nil)
 }
 
 func (l *LudoService) DeleteItem(id uint) error {
-	return nil
+	return l.request("DELETE", fmt.Sprintf("/items/%d", id), nil, nil)
 }
 
 func (l *LudoService) MoveItemToList(itemId, list uint) error {
-	return nil
+	return l.request("PATCH", fmt.Sprintf("/items/%d/move/%d", itemId, list), nil, nil)
 }
